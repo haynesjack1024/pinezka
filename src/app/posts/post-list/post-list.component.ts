7455c45f2a343681ba1b1f436031b1c1b698f659
Jack Jonathan Haynes <haynesjack@startmail.com>
2025-01-25 12:12:50 +0100

    filter posts in post-list based on search and category query params

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -5,6 +5,13 @@ import { PostItemComponent } from '../post-item/post-item.component';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { SearchBarComponent } from '../../forms/search-bar/search-bar.component';
 import { FilteringSidebarComponent } from '../../forms/filtering-sidebar/filtering-sidebar.component';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { combineLatest, map } from 'rxjs';
+
+type PostFilteringFn = ([posts, params]: [Post[], ParamMap]) => [
+  Post[],
+  ParamMap,
+];
 
 @Component({
   selector: 'app-post-list',
@@ -17,13 +24,51 @@ export class PostListComponent implements OnInit {
 
   public constructor(
     private postService: PostService,
+    private route: ActivatedRoute,
     private destroyRef: DestroyRef,
   ) {}
 
   public ngOnInit(): void {
-    this.postService
-      .getPosts()
-      .pipe(takeUntilDestroyed(this.destroyRef))
+    combineLatest([this.postService.getPosts(), this.route.queryParamMap])
+      .pipe(
+        map(this.filterByCategory),
+        map(this.filterByText),
+        map(([posts]) => posts),
+        takeUntilDestroyed(this.destroyRef),
+      )
       .subscribe((posts) => (this.posts = posts));
   }
+
+  private readonly filterByCategory: PostFilteringFn = ([posts, params]) => {
+    const category = parseInt(params.get('category') ?? '');
+
+    return [
+      !isNaN(category)
+        ? posts.filter((post) => post.category === category)
+        : posts,
+      params,
+    ];
+  };
+
+  private readonly filterByText: PostFilteringFn = ([posts, params]) => {
+    const search = (params.get('search') ?? '').toLowerCase().split(' ');
+
+    return [
+      search.length
+        ? posts.filter((post) => {
+            const postData = [
+              post.author.username,
+              post.author.email,
+              post.title,
+              post.content,
+            ].map((data) => data.toLowerCase());
+
+            return postData.some((data) =>
+              search.every((token) => data.includes(token)),
+            );
+          })
+        : posts,
+      params,
+    ];
+  };
 }
